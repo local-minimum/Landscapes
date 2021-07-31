9a9acc6d010ca77a16862e4688f4a8d99be4c261
Martin Zackrisson <martin.zackrisson@fripost.org>
2021-07-31 12:41:40 +0200

    A flat grid

---
+++ Assets/Scripts/GeoNode.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoNode : MonoBehaviour
+{
+    List<GeoNode> neighbours = new List<GeoNode>();
+    public Geography geography { get; set; }
+    public float gizmoSize { get; set; }
+
+    public void SetNeighbours(List<GeoNode> nodes)
+    {
+        if (neighbours.Count == 0)
+        {
+            neighbours.AddRange(nodes);
+        } else
+        {
+            throw new System.InvalidOperationException(
+                "Neighbours may only be set once"
+            );
+        }
+    }
+
+    public float SqDistance(GeoNode neighbour)
+    {
+        for (int i = 0, l = neighbours.Count; i < l; i++)
+        {
+            if (neighbours[i] == neighbour)
+            {
+                return Vector3.SqrMagnitude(neighbour.transform.position - transform.position);
+            }
+        }
+        throw new System.ArgumentException("Not a neighbour", nameof(neighbour));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = transform.position.y < 0 ? Color.blue : Color.green;
+        Gizmos.DrawSphere(transform.position, gizmoSize);
+        Gizmos.color = Color.white;
+        for (int i = 0, l = neighbours.Count; i < l; i++)
+        {
+            GeoNode neighbour = neighbours[i];            
+            if (neighbour.transform.position.x < transform.position.x)
+            {
+                Gizmos.DrawLine(transform.position, neighbour.transform.position);
+            } else if (neighbour.transform.position.x == transform.position.x && neighbour.transform.position.z < transform.position.z) {
+                Gizmos.DrawLine(transform.position, neighbour.transform.position);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Geography.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Geography : MonoBehaviour
+{
+    public int width = 30;
+    public int height = 20;
+    public float spacing = 1;
+
+    List<GeoNode> nodes = new List<GeoNode>();
+
+    private void Start()
+    {
+        CreateGrid();
+        ConnectGrid();
+    }
+
+    void CreateGrid()
+    {
+        float halfWidth = width / 2.0f;
+        float halfHeight = height / 2.0f;
+        for (int x = 0; x<width; x++)
+        {
+            for (int z=0; z<height;z++)
+            {
+                Vector3 pos = new Vector3(
+                    transform.position.x - (x - halfWidth) * spacing,
+                    transform.position.y,
+                    transform.position.z - (z - halfHeight) * spacing
+                );
+                nodes.Add(SpawnGeoNode(pos));
+            }
+        }
+    }
+
+    GeoNode SpawnGeoNode(Vector3 position)
+    {
+        var go = new GameObject(string.Format("GeoNode #{0}", nodes.Count));
+        go.transform.SetParent(transform);
+        go.transform.position = position;
+        var node = go.AddComponent<GeoNode>();
+        node.geography = this;
+        node.gizmoSize = spacing / 5f;
+        return node;
+    }
+
+    void ConnectGrid()
+    {
+        float connectLengthSq = 2.001f * spacing * spacing;
+        int nNodes = nodes.Count;
+        for (int i=0; i<nNodes; i++)
+        {
+            GeoNode node = nodes[i];
+            List<GeoNode> neighbours = new List<GeoNode>();
+            for (int j=0; j<nNodes; j++)
+            {
+                if (i == j) continue;
+                if (Vector3.SqrMagnitude(nodes[j].transform.position - node.transform.position) < connectLengthSq)
+                {
+                    GeoNode other = nodes[j];
+                    if (other.transform.position.z < node.transform.position.z && other.transform.position.x < node.transform.position.x)
+                    {
+                        continue;
+                    }
+                    neighbours.Add(other);
+                }
+            }
+            node.SetNeighbours(neighbours);
+        }
+    }
+}
