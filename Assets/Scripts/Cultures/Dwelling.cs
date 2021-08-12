0199a8e175202c1d436040e2af1d945380da9ad6
Martin Zackrisson <martin.zackrisson@fripost.org>
2021-08-12 11:58:16 +0200

    Dwellings

---
+++ Assets/Scripts/Cultures/Dwelling.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class Dwelling : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        Geography.OnWorldReady += Geography_OnWorldReady;
+    }
+
+    private void OnDisable()
+    {
+        Geography.OnWorldReady -= Geography_OnWorldReady;
+    }
+    private void Geography_OnWorldReady(Geography geography)
+    {
+        CreateMesh();
+    }
+
+    void CreateMesh()
+    {
+        var mf = GetComponent<MeshFilter>();
+        var mesh = new Mesh();
+        mesh.name = "Dwelling";
+        mesh.vertices = new Vector3[]
+        {
+                new Vector3 (-1, 0, -1),
+                new Vector3 (1, 0, -1),
+                new Vector3 (1, 3, -1),
+                new Vector3 (-1, 3, -1),
+                new Vector3 (-1, 3, 1),
+                new Vector3 (1, 3, 1),
+                new Vector3 (1, 0, 1),
+                new Vector3 (-1, 0, 1),
+        };
+        mesh.triangles = new int[]
+        {
+                0, 2, 1, //face front
+                0, 3, 2,
+                2, 3, 4, //face top
+                2, 4, 5,
+                1, 2, 5, //face right
+                1, 5, 6,
+                0, 7, 4, //face left
+                0, 4, 3,
+                5, 4, 7, //face back
+                5, 7, 6,
+                0, 6, 7, //face bottom
+                0, 1, 6
+        };
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();        
+        mf.sharedMesh = mesh;
+        var mr = GetComponent<MeshRenderer>();
+        mr.material = Resources.Load<Material>("DwellingMatA");
+        
+    }
+}
---
+++ Assets/Scripts/Landscapers/Settler.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Settler : LandscaperBase
+{
+    [SerializeField]
+    int settlements = 20;
+
+    [SerializeField]
+    float maxWeight = 100;
+    [SerializeField]
+    float minWeightSelected = 5;
+
+    [SerializeField]
+    float shoreWeight = 1f;
+
+    [SerializeField]
+    float openSeaWeight = 3f;
+
+    [SerializeField]
+    float landBaseWeight = 1.5f;
+
+    [SerializeField]
+    float mountainWeight = 5f;
+
+    [SerializeField]
+    float deltaHeightForMountain = 2f;
+
+    Dictionary<NodeBase, float> nodeWeights = new Dictionary<NodeBase, float>();
+
+    protected override IEnumerator<float> Landscape(Geography geography)
+    {
+        var candidates = GetCandidates(geography);
+
+        for (int i = 0; i<settlements; i++)
+        {
+            var settlement = GetSettlement(candidates);
+            candidates.Remove(settlement);
+            settlement.gameObject.AddComponent<Dwelling>();
+            RecalculateWeights(settlement);
+            if (i % 5 == 0) yield return (i + 1f) / settlements;
+        }
+    }
+
+    List<GeoNode> GetCandidates(Geography geography)
+    {
+        var candidates = geography
+            .GetNodes(n => { var topo = n.topology; return topo.HasFlag(NodeBase.Topology.Land) && topo.HasFlag(NodeBase.Topology.Shore); })
+            .ToList();
+        for (int i = 0, l=geography.NodeCount; i<l; i++)
+        {
+            nodeWeights[geography.GetNode(i)] = maxWeight;
+        }
+        return candidates;
+    }
+
+    GeoNode GetSettlement(List<GeoNode> candidates)
+    {
+        float totalWeight = 0;
+        for (int i = 0, l = candidates.Count; i < l; i++)
+        {
+            totalWeight += nodeWeights[candidates[i]];
+        }
+        totalWeight *= Random.value;
+        for (int i = 0, l = candidates.Count; i < l; i++)
+        {
+            var candidate = candidates[i];
+            var weight = nodeWeights[candidate];
+            if (totalWeight <= weight) return candidate;
+            totalWeight -= weight;
+        }
+        return candidates.Last();
+    }
+
+    void RecalculateWeights(GeoNode settlement)
+    {
+        nodeWeights[settlement] = 0;
+        var nodes = new List<GeoNode>() { settlement };
+        var visited = new HashSet<GeoNode>();
+        while (nodes.Count > 0)
+        {
+            var node = nodes[0];
+            nodes.RemoveAt(0);
+            visited.Add(node);
+            foreach (var neighbour in node.GetNeighbours((n, neigh) => !visited.Contains(neigh)))
+            {
+                var weight = nodeWeights[node];
+                var topo = neighbour.topology;
+                if (topo.HasFlag(NodeBase.Topology.Water))
+                {
+                    if (topo.HasFlag(NodeBase.Topology.Shore))
+                    {
+                        weight += shoreWeight;
+                    } else
+                    {
+                        weight += openSeaWeight;
+                    }
+                } else
+                {
+                    if (topo.HasFlag(NodeBase.Topology.Shore))
+                    {
+                        weight += shoreWeight;
+                    } else
+                    {
+                        weight += landBaseWeight;
+                    }
+                    if (Mathf.Abs(Mathf.Max(0, node.Elevation) - neighbour.Elevation) > deltaHeightForMountain)
+                    {
+                        weight += mountainWeight;
+                    }
+                }
+                if (nodeWeights[neighbour] > weight)
+                {
+                    nodeWeights[neighbour] = nodeWeights[neighbour];
+                    nodes.Add(neighbour);
+                    nodes.Sort((a, b) => nodeWeights[a] - nodeWeights[b] < 0 ? -1 : 1);
+                }                                
+            }
+        }
+    }
+}
+
