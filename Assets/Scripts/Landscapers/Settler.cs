bc757432eccbadea4069b66fafa7696db6f11363
Martin Zackrisson <martin.zackrisson@fripost.org>
2021-08-12 13:40:56 +0200

    Creating settlements

--- Assets/Scripts/Landscapers/Settler.cs
+++ Assets/Scripts/Landscapers/Settler.cs
@@ -28,6 +28,15 @@ public class Settler : LandscaperBase
     [SerializeField]
     float deltaHeightForMountain = 2f;
 
+    [SerializeField]
+    float bonusWeightThreshold = 50f;
+
+    [SerializeField]
+    int bonusSearchDepth = 3;
+
+    [SerializeField]
+    float bonusMultiplier = 3f;
+
     Dictionary<NodeBase, float> nodeWeights = new Dictionary<NodeBase, float>();
 
     protected override IEnumerator<float> Landscape(Geography geography)
@@ -58,20 +67,54 @@ public class Settler : LandscaperBase
 
     GeoNode GetSettlement(List<GeoNode> candidates)
     {
+        var weights = new float[candidates.Count];
         float totalWeight = 0;
         for (int i = 0, l = candidates.Count; i < l; i++)
-        {
-            totalWeight += nodeWeights[candidates[i]];
+        {            
+            var candidate = candidates[i];
+            var weight = nodeWeights[candidate];
+            if (weight < minWeightSelected) continue;
+            var bonus = CalculateBonus(candidate);
+            weights[i] = bonus + weight;
+            totalWeight += bonus + weight;
         }
         totalWeight *= Random.value;
         for (int i = 0, l = candidates.Count; i < l; i++)
         {
             var candidate = candidates[i];
-            var weight = nodeWeights[candidate];
+            var weight = weights[i];
             if (totalWeight <= weight) return candidate;
             totalWeight -= weight;
         }
         return candidates.Last();
+    }
+
+    float CalculateBonus(GeoNode node)
+    {
+        
+        var tiles = new List<GeoNode>() { node };
+        var depths = new Dictionary<GeoNode, int>();
+        depths[node] = 0;
+        var j = 0;
+        while (j < tiles.Count)
+        {
+            var curNode = tiles[j];
+            if (depths[curNode] >= bonusSearchDepth)
+            {
+                j++;
+                continue;
+            }
+            foreach (var neighbour in curNode.GetNeighbours((n, neigh) => !depths.ContainsKey(neigh))) {
+                if (
+                    nodeWeights[neighbour] <= bonusWeightThreshold
+                    || Mathf.Abs(Mathf.Max(0, curNode.Elevation) - Mathf.Max(0, neighbour.Elevation)) >= deltaHeightForMountain
+                ) continue;
+                tiles.Add(neighbour);
+                depths[neighbour] = depths[curNode] + 1;
+            }
+            j++;                        
+        }
+        return (tiles.Count - 1) * bonusMultiplier;
     }
 
     void RecalculateWeights(GeoNode settlement)
